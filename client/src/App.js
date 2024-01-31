52d538108bd3cf27a1bb4c1607ee10aae44481fc
Chanaka Wickramasinghe <50458821+chanakawickramasinghe@users.noreply.github.com>
2024-01-31 10:15:14 +0530

    2023B2

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MessageSender from './Components/MessegeSender';
+
+function App() {
+  return (
+    <div className="App">
+      <MessageSender/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/MessegeSender.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const MessageSender = () => {
+  const [message, setMessage] = useState('');
+  const [response, setResponse] = useState('');
+
+  const sendToServer = () => {
+    axios.post('http://localhost:3001/change-message', { message })
+      .then(res => setResponse(res.data.message))
+      .catch(() => setResponse('Error sending to server'));
+  };
+
+  return (
+    <div>
+      <input type="text" value={message} onChange={e => setMessage(e.target.value)} />
+      <button onClick={sendToServer}>Send</button>
+      <div>{response}</div>
+    </div>
+  );
+};
+
+export default MessageSender;
